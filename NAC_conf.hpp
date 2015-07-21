c133af1394f0f71de9fa2c91cea6c9c6b226a8a6
Paige <paige.codes@gmail.com>
2015-07-21 12:37:16 -0400

    Create NAC_conf.hpp
    Header file where system specific parameters are defined.

---
+++ NAC_conf.hpp
@@ -0,0 +1,13 @@
+#ifndef _NAC_CONF_HPP
+#define _NAC_CONF_HPP
+#define LOCAL_HOST "NACmgr"
+#define LOCAL_DB_HOST "192.168.32.10"
+#define NAC1 "10.33.244.22"
+#define NAC2 "10.33.144.22"
+#define NACmgr "10.33.44.22"
+#define LOG "/usr/local/NACMGR/var/logs/polling.log"
+#define NACmgr_debug_file "/usr/local/NACMGR/var/logs/log.messages"
+#define TASK_DIR "/usr/local/NACMGR/bin"
+#define TASK_FILE "/usr/local/NACmgr/var/NACmgr_tasks"
+#define ARCHIVES_DIR "/usr/local/NACmgr/archive"
+#endif
